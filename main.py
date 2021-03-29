3b8a2dd45a15a61ac9909699741869460ed1e7dd
Oliver Berger <oliver@digitalarchitekt.de>
2021-03-29 18:05:47 +0200

    dsdfsa

--- main.py
+++ main.py
@@ -16,14 +16,20 @@ http = urllib3.PoolManager()
 
 
 def get_artifact(name):
+    print(f"Download `{name}` from: {artifacts_url}")
     r = http.request("GET", artifacts_url, headers=headers)
     data = json.loads(r.data.decode("utf-8"))
     for artifact in data["artifacts"]:
+        print("Check artifact", artifact["name"])
         if artifact["name"] == name:
             return artifact
 
 
-def download_artifact(artifact):
+def download_artifact(name):
+    artifact = get_artifact(name)
+    if artifact is None:
+        print(f"::set-output name=error::Artifact not found: {name}")
+
     r = http.request("GET", artifact["archive_download_url"], headers=headers)
     with open(artifact["name"], "wb") as f:
         f.write(r.data)
@@ -31,4 +37,4 @@ def download_artifact(artifact):
 
 
 if __name__ == "__main__":
-    download_artifact(get_artifact(artifact_name))
+    download_artifact(artifact_name)
